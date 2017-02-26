c7be95645df5e333bda428c451fd09e62c483be8
William Menten-Weil <wtmenten@gmail.com>
2017-02-25 18:41:09 -0800

    fixed the grouping by domain

--- featureFunctions.js
+++ featureFunctions.js
@@ -17,40 +17,87 @@ var groupFeatures = {
     // special case for smart-grouping
     byDomain: function(data, options) {
         var groups = [];
+        var sortFuncs =  [
+            function(val){ return val.range[0]},
+            function(val){ return val.range[1] - val.range[0]},
+        ];
+
         for (var i = 0, iLen = data.length; i < iLen; i++) {
             var col = data[i];
-            var localMin = _.min(col.values);
-            var localMax = _.max(col.values);
-            for (var j = 0, jLen = groups.length; i < jLen; j++) {
-                var group = groups[j];
-                var isWithinAcceptableMin = localMin >= group.range[0] * 0.5;
-                var isWithinAcceptableMax = localMax <= group.range[1] * 1.5;
-                if (isWithinAcceptableMin && isWithinAcceptableMax) {
-                    group.features.push(col);
-                    if (localMin < group.range[0]) {
-                        group.range[0] = localMin;
-                        group.name = group.range.toString();
-                    }
-                    if (localMax > group.range[1]) {
-                        group.range[1] = localMax;
-                        group.name = group.range.toString();
-                    }
-                } else {
-                    var newRange = [localMin,localMax]
-                    var newGroup = {
-                        name: newRange.toString(),
-                        range: newRange,
-                        features: [col]
-                    }
-                    groups.push(newGroup);
+            var localMin = _.minBy(col.values, function(d){ return d.val;}).val;
+            var localMax = _.maxBy(col.values, function(d){ return d.val;}).val;
+            if (groups.length == 0) {
+                var newRange = [localMin,localMax]
+                var newGroup = {
+                    name: newRange.toString(),
+                    range: newRange,
+                    features: [col]
                 }
-                var sortFuncs =  [
-                    function(val){ return val.range[1] - val.range[0]},
-                    function(val){ return val.range[0]}
-                ];
-                groups = _.sortBy(groups,sortFuncs);
+                groups.push(newGroup);
+            } else {
+                var unfound = true;
+                for (var j = 0, jLen = groups.length; j < jLen; j++) {
+                    var group = groups[j];
+                    var minTest, maxTest;
+                    var tgMin = group.range[0];
+                    var tgMax = group.range[1];
+                    var tlMin = localMin;
+                    var tlMax = localMax;
+                    // var tgMin = Math.log(group.range[0]);
+                    // var tgMax = Math.log(group.range[1]);
+                    // var tlMin = Math.log(localMin);
+                    // var tlMax = Math.log(localMax);
+                    minRatio = Math.abs((tlMin+0.001)/(tgMin+0.001))
+                    minTest = minRatio > .4 && minRatio < 1.6;
+                    maxRatio = Math.abs((tlMax+0.001)/(tgMax+0.001))
+                    maxTest = maxRatio > .4 && maxRatio < 1.6;
+                    // if (group.range[1] < 0)  {
+                    //     maxTest = tgMax * 0.5;
+                    // } else {
+                    //     maxTest = tgMax * 2;
+                    // }
+                    // var groupRange = tgMax - tgMin;
+                    // var localRange = tlMax - tlMin;
+
+                    // var rangeTest;
+                    // if (groupRange > localRange) {
+                    //     rangeTest = localRange * 2 > groupRange;
+                    // } else {
+                    //     rangeTest = localRange > groupRange * 2;
+                    // }
+
+                    var isWithinAcceptableMin = minTest;
+                    var isWithinAcceptableMax = maxTest;
+                    if (isWithinAcceptableMin && isWithinAcceptableMax) {
+                        group.features.push(col);
+                        unfound = false;
+                        if (localMin < group.range[0]) {
+                            group.range[0] = localMin;
+                            group.name = group.range.toString();
+                        }
+                        if (localMax > group.range[1]) {
+                            group.range[1] = localMax;
+                            group.name = group.range.toString();
+                        }
+                        break;
+                    }
+                } // end group loop
+                if (unfound) {
+                        var newRange = [localMin,localMax]
+                        var newGroup = {
+                            name: newRange.toString(),
+                            range: newRange,
+                            features: [col]
+                        }
+                        groups.push(newGroup);
+                        groups = _.sortBy(groups,sortFuncs);
+                }
             }
-        }
+
+        } // end data loop
+
+        groups = _.sortBy(groups,sortFuncs);
+        return groups;
     },
 
     byRegex: function(data, regexExpr) {
